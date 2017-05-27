8e806d8ac3111e2757e28aa2a84552c46596c343
Anthony Cajacuri <ancaor_s1@hotmail.com>
2017-05-27 16:05:36 -0500

    Update media2.php

--- media2.php
+++ media2.php
@@ -3,7 +3,6 @@ $rango = $_POST['rango'];
 $xi = $_POST['xi'];
 $fi = $_POST['fi'];
 
-
 $cantidad = count($xi);
 
 $casimedia = 0;
@@ -11,9 +10,7 @@ echo "<p> Los datos Son: </p>";
 for ($i=0; $i < $cantidad; $i++) { 
 	$casimedia = $casimedia + ($xi[$i] * $fi[$i]);
 }
-
 echo "<p>Las sumas de x<sub>i</sub> * f<sub>i</sub> = ".$casimedia."</p>";
-
 $sumaf = 0;
 for ($i=0; $i < $cantidad; $i++) { 
 	$sumaf = $sumaf + $fi[$i];
